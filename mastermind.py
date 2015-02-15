62263b74796b26911cfa55711c58a745f7503df0
Pietdagamer <Pietdagamer@users.noreply.github.com>
2015-02-15 21:20:15 +0100

    >:(

--- mastermind.py
+++ mastermind.py
@@ -1,17 +1,17 @@
 from random import randint
 class mastermindGame:
-    def __init__(self, M, L, W, pos):
-        self.M = M
-        self.L = L
-        self.W = W
+    def __init__(self, MAX, LENGTH, W, pos):
+        self.MAX = MAX
+        self.LENGTH = LENGTH
+        self.WIDTH = WIDTH
         self.pos = pos
-        self.guessesLeft = self.M
+        self.guessesLeft = self.MAX
         self.ans = generateRandomAnswer()
     
     def generateRandomAnswer(self):
         posNotUsed = pos
         ans = ""
-        for i in range(0, L):
+        for i in range(0, LENGTH):
             char = posNotUsed[randint(0,len(posNotUsed) - 1)]
             posNotUsed = posNotUsed.replace(char, "")
             ans += char
